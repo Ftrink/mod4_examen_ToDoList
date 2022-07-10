f101418db9a36f1acf2c79ccb36d8d67617ff472
Felipe Trincado <ftrincado7@gmail.com>
2022-07-10 14:17:24 -0400

    js completo

--- todolist.js
+++ todolist.js
@@ -2,14 +2,8 @@
 container = document.querySelector('.container');
 input_principal = document.querySelector('.input');
 boton_agregar = document.querySelector('.boton-agregar')
-<<<<<<< HEAD
-h1nombre = document.querySelector('#nombrehero')
 
 // crear class
-=======
-
-// crear classItem
->>>>>>> 8266f667a2646fdafcf0d27529a9674dadcc6a27
 class Item {
   constructor(nuevaTarea) {
     this.crearDiv(nuevaTarea)
@@ -24,71 +18,14 @@ class Item {
     inputItem.value = nuevaTarea
 
     //div
-<<<<<<< HEAD
     const div_item = document.createElement('div')
     div_item.classList.add('Item')
-=======
-    const divs_item = document.createElement('div')
-    divs_item.classList.add('Item')
 
     // btn editar
     let botonEditar = document.createElement('button');
     botonEditar.innerHTML = '<i class="fa-solid fa-lock">';
     botonEditar.classList.add('boton-editar');
 
-    //btn removeeer
-    let botonRemover = document.createElement('button');
-    botonRemover.classList.add('boton-remover');
-    botonRemover.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
-
-    //agregar elementos a la lista
-    divs_item.appendChild(inputItem)
-    divs_item.appendChild(botonEditar)
-    divs_item.appendChild(botonRemover)
-    container.appendChild(divs_item);
-
-    //eventos botones edit
-    botonEditar.addEventListener('click', function () {
-      if (inputItem.disabled == true) {
-        botonEditar.innerHTML = '<i class="fas fa-lock-open"></i>';
-        inputItem.disabled = !true
-      } else {
-        inputItem.disabled = !false
-        botonEditar.innerHTML = '<i class="fa-solid fa-lock">'
-      }
-    })
-
-    //remove
-    botonRemover.addEventListener('click', function () {
-      container.removeChild(divs_item)
-
-    })
-
-  }
-};
-
-//check input
-function chequearInput() {
-  if (input_principal.value == '') {
-    return
-  } else {
-    const res = new Item(input_principal.value.trim());
-    input_principal.value = '';
-    // return res
-  }
-};
-
-boton_agregar.addEventListener('click', function () {
-  chequearInput()
-});
->>>>>>> 8266f667a2646fdafcf0d27529a9674dadcc6a27
-
-    // btn editar
-    let botonEditar = document.createElement('button');
-    botonEditar.innerHTML = '<i class="fa-solid fa-lock">';
-    botonEditar.classList.add('boton-editar');
-
-<<<<<<< HEAD
     //btn removeeer
     let botonRemover = document.createElement('button');
     botonRemover.classList.add('boton-remover');
@@ -173,9 +110,3 @@ const removeLs = function (tarea) {
   arrayLS = arrayLS.filter(x => x != tarea);
   localStorage.setItem('tarea', JSON.stringify(arrayLS));
 };
-=======
-// debugger
-// console.log('creando ...');
-const stein2 = new Item()
-const stein3 = new Item()
->>>>>>> 8266f667a2646fdafcf0d27529a9674dadcc6a27
